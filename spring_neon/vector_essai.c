200e25e48ba65a571e6c74fb55f70053d272c05f
ClaireDlp <delaplace.claire@gmail.com>
2016-02-09 15:16:01 +0100

    movemask neon essai

--- spring_neon/vector_essai.c
+++ spring_neon/vector_essai.c
@@ -30,19 +30,17 @@ typedef int32x2_t sv32 __attribute__((aligned (16)));
 #define v8_transpose(x) vtrn_s8(v8_lside(x), v8_rside(x))
 
 
-/* #define v16_movemask(x) ({						\ */
-/*       int8x16_t q0 = vreinterpretq_s8_s16(a);				\ */
-/*       int8x8x2_t q1 = vtrn_s8(vget_high_s8(q0), vget_low_s8(q0));	\ */
-/*       int32x2_t d = vreinterpret_s32_s8(q1.val[1]);			\ */
-/*       d[0]|d[1];							\ */
-/*     }) */
+#define v16_movemask(x) ({				      	\	
+dsv8 q0 = v8_transpose(v16_to_v8(x));				\
+sv32 q1 = vreinterpret_s32_s8(q0.val[0]);			\
+q1[0]|q1[1];							\
+})
 
 
 int main(){
   v16 a;
   v8 b;
   dsv8 tr;
-
   int i, mask;
 
   for(i = 0; i < 8; i++){
@@ -54,11 +52,13 @@ int main(){
 
   b = v16_to_v8(a);
 
-  for(i = 0; i <16 ; i++){
-    printf("b[%d] = %d\n", i, b[i]);
-  }
+  /* for(i = 0; i <16 ; i++){ */
+  /*   printf("b[%d] = %d\n", i, b[i]); */
+  /* } */
 
   tr = v8_transpose(b);
+
+
 
   /* for(i = 0; i < 8; i++){ */
   /*   printf("tr0[%d] = %d ; tr1[%d] = %d\n", i, tr.val[0][i], i, tr.val[1][i]); */
@@ -66,9 +66,9 @@ int main(){
 
   //c = v16_add(a,b);
 
-  // mask = v16_movemask(a);
+  mask = v16_movemask(a);
 
-  // printf("mask = %x", mask);
+  printf("mask = %x", mask);
   /* for(i = 0; i < 8; i++){ */
   /*   printf("c[%d] = %d\n", i, c[i]); */
   /* } */
