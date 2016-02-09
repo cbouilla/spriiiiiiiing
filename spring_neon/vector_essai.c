e10b87f5cb240a536ab36bfb5352c34ba21ae6ce
ClaireDlp <delaplace.claire@gmail.com>
2016-02-09 14:30:24 +0100

    movemask macro essai

--- spring_neon/vector_essai.c
+++ spring_neon/vector_essai.c
@@ -13,20 +13,36 @@ typedef int16x8_t v16 __attribute__ ((aligned (16)));
 #define v16_sub vsubq_s16
 #define v16_mul vmulq_s16
 
+#define v16_shift_r  vshrq_n_s16
+#define v16_shift_l  vshlq_n_s16
+
+#define v16_movemask(x) ({						\
+      int8x16_t q0 = vreinterpretq_s8_s16(a);				\
+      int8x8x2_t q1 = vtrn_s8(vget_high_s8(q0), vget_low_s8(q0));	\
+      int32x2_t d = vreinterpret_s32_s8(q1.val[1]);			\
+      d[0]|d[1];							\
+    })
+
+
 int main(){
-  v16 a, b, c;
-  int i;
+  v16 a;
+  int i, mask;
 
   for(i = 0; i < 8; i++){
-    a[i] = 1;
-    b[i] = i;
+    a[i] = 0;
   }
 
-  c = v16_add(a,b);
+  a[0] = 1;
+  a[7] = 1;
 
-  for(i = 0; i < 8; i++){
-    printf("c[%d] = %d\n", i, c[i]);
-  }
+  //c = v16_add(a,b);
+
+  mask = v16_movemask(a);
+
+  printf("mask = %x", mask);
+  /* for(i = 0; i < 8; i++){ */
+  /*   printf("c[%d] = %d\n", i, c[i]); */
+  /* } */
 
 }
 
