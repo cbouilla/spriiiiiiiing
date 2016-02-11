5ae6397ac43a7eadeaeeca5a48cb65c00dbfc716
ClaireDlp <delaplace.claire@gmail.com>
2016-02-11 14:47:04 +0100

    tentative de debugage de fft128

--- spring_neon/vector.c
+++ spring_neon/vector.c
@@ -372,17 +372,16 @@ static inline void fft128(void *a) {
   //  v16 *Twiddle = (v16*)FFT128_Twiddle;
 
   /* Size-2 butterflies */
-  printf("DEBUG L375\n");
   for (i = 0; i<8; i++) {
+    printf("A[%d] = %d\n", i, A[i]);
+    printf("A[%d] = %d\n", i+8, A[i+8]);
     B[i]   = v16_add(A[i], A[i+8]);
     A[i+8] = v16_sub(A[i], A[i+8]);
-    printf("DEBUG L379 i : %d\n", i);
     A[i+8] = REDUCE_FULL(A[i+8]);
     A[i+8] = v16_mul(A[i+8], FFT128_Twiddle[i]);
     A[i+8] = REDUCE(A[i+8]);
   }
 
-  printf("DEBUG L384\n");
   B[3] = REDUCE(B[3]);
   B[7] = REDUCE(B[7]);
 
